Finetuned plotting routines and fixed bug in computation of wthlvp mean gradient production

stats3d_eco.py
@@ -436,7 +436,7 @@ for i in range(len(plttime)):
     
     # wthlv
     wthlvpf_prod = lowPass(wfp**2,circ_mask)[1:-1]*Gamma_thlv_f[:,np.newaxis,np.newaxis]
-    wthlvp_prod_av = np.mean(wthlvpf_prod,axis=(1,2))
+    wthlvp_prod_av = np.mean((wfp**2)[1:-1]*Gamma_thlv_f[:,np.newaxis,np.newaxis],axis=(1,2))
     wthlvpf_prod_moist_time[i,:] = mean_mask(wthlvpf_prod, mask_moist) - wthlvp_prod_av
     wthlvpf_prod_dry_time[i,:] = mean_mask(wthlvpf_prod, mask_dry) - wthlvp_prod_av
     

stats3d_eco_load.py
@@ -152,7 +152,7 @@ Gamma_qt = qtpf_prod_moist_wex_time/wff_moist_time[:,1:-1]
 
 #%% Plotprofiles of  mesoscale-filtered variables in time
 tpltmin = 6.
-tpltmax = 20.
+tpltmax = 23.
 dit = 1.0 # Rounds to closest multiple of dt in time
 
 itpltmin = np.where(time[plttime]>=tpltmin)[0][0]
@@ -242,8 +242,8 @@ axs[3].legend(loc='best',bbox_to_anchor=(1,1),ncol=len(plttime_var)//13+1)
 
 #%%
 # Average budget contributions over time dimension
-tpltmin = 12.
-tpltmax = 16.
+tpltmin = 16.
+tpltmax = 23.
 
 itpltmin = np.where(time[plttime]>=tpltmin)[0][0]
 itpltmax = np.where(time[plttime]<tpltmax)[0][-1]+1