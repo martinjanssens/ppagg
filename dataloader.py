49d99c844e671f933ca474e90440db9044e383ee
martinjanssens <martinjanssens94@hotmail.com>
2022-10-24 13:20:24 +0200

    1D budget added

--- dataloader.py
+++ dataloader.py
@@ -127,7 +127,22 @@ class DataLoaderDALES:
     
     def load_thv2av(self, izmin, izmax):
         return np.ma.getdata(self.ds1.variables['thv2r'][:,izmin:izmax])
-
+    
+    def load_prec(self, izmin, izmax):
+        if 'qtptot' in self.ds1.variables.keys():
+            return np.ma.getdata(self.ds1.variables['qtptot'][:,izmin:izmax])
+        else:
+            print('No precipitation tendency stored, returning zeros')
+            return np.zeros((time1d.size, izmax-izmin))
+    
+    def load_radi(self, izmin, izmax):
+        if 'thltend' in self.ds1.variables.keys():
+            return np.ma.getdata(self.ds1.variables['thltend'][:,izmin:izmax])
+        else:
+            print('No radiation tendency stored, returning zeros')
+            return np.zeros((time1d.size, izmax-izmin))
+    
+    
 class DataLoaderDALESSeparate:
 
     def __init__(self, load_path):
@@ -245,6 +260,19 @@ class DataLoaderDALESSeparate:
     def load_thv2av(self, izmin, izmax):
         return np.ma.getdata(self.ds1.variables['thv2r'][:,izmin:izmax])
     
+    def load_prec(self, izmin, izmax):
+        if 'qtptot' in self.ds1.variables.keys():
+            return np.ma.getdata(self.ds1.variables['qtptot'][:,izmin:izmax])
+        else:
+            print('No precipitation tendency stored, returning zeros')
+            return np.zeros((time1d.size, izmax-izmin))
+    
+    def load_radi(self, izmin, izmax):
+        if 'thltend' in self.ds1.variables.keys():
+            return np.ma.getdata(self.ds1.variables['thltend'][:,izmin:izmax])
+        else:
+            print('No radiation tendency stored, returning zeros')
+            return np.zeros((time1d.size, izmax-izmin))
 
 class DataLoaderMicroHH:
 
@@ -363,3 +391,10 @@ class DataLoaderMicroHH:
     def load_thv2av(self, izmin, izmax):
         return np.ma.getdata(self.ds1['thermo']['thv_2'][:,izmin:izmax])
     
+    def load_prec(self, izmin, izmax):
+        print('Warning: You have not implemented output precipitation tendencies. Returning zeros...')
+        return np.zeros((time1d.size, izmax-izmin))
+    
+    def load_radi(self, izmin, izmax):
+        print('Warning: You have not implemented output precipitation tendencies. Returning zeros...')
+        return np.zeros((time1d.size, izmax-izmin))
